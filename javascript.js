4fd49c05ff143ce26c5a29db4a32a4cb55dc21b3
Zotti39 <gabriel.zzanatta03@gmail.com>
2023-10-19 13:27:03 -0300

    The calculator now calculate(punch), but it only uses the eval() function, there was also added two functions, one to erase the last operator/number added, and one to start a new calculation.

--- javascript.js
+++ javascript.js
@@ -1,11 +1,46 @@
 const contas = document.querySelector('.contas');
-const resultado =document.querySelector('.resultado');
+const resultado = document.querySelector('.resultado');
 let expressaoFinal = '';
 
-function visor(){
-    contas.textContent = expressaoFinal;
+function resultadoVisor(x){
+    resultado.textContent = x;
+}
+function visor(y){
+    contas.textContent = y;
 }
 function appendAlg(valor){
     expressaoFinal += valor;
-    visor();
+    visor(expressaoFinal);
 };
+function calculate(expressao){
+    try{
+        const resultado = eval(expressao);
+        resultadoVisor(resultado);
+        console.log(`${resultado} is the answer, succesful calculation!`);
+    }
+    catch(error){
+        resultadoVisor('Error!');
+        console.log('Found an error on the calculations!');
+    }
+}
+function apagar(){
+    expressaoFinal = '';
+    visor(expressaoFinal);
+    resultadoVisor(expressaoFinal);
+    console.log('Calculations erased succesfully!');
+}
+function corrige(z){
+    let newZ = '';
+    if (z[z.length - 1] == ' '){
+        newZ = z.slice(0, -3);
+        z = newZ;
+        console.log('if works');
+    }
+    else{
+        newZ = z.slice(0, -1);
+        z = newZ;
+        console.log('else works');
+    }
+    expressaoFinal = z;
+    visor(expressaoFinal);
+}
