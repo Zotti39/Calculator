fc25619da435219a33500d9ada66e53577afe1f5
Zotti39 <gabriel.zzanatta03@gmail.com>
2023-10-20 13:25:17 -0300

    halfway through establishing a substitute for the eval() function, not done yet

--- javascript.js
+++ javascript.js
@@ -11,7 +11,53 @@ function visor(y){
 function appendAlg(valor){
     expressaoFinal += valor;
     visor(expressaoFinal);
-};
+}
+function add(a, b){
+    return parseInt(a) + parseInt(b);
+}
+function subtract(a,b){
+    return parseInt(a) - parseInt(b);
+}
+function multiply(a,b){
+    return parseInt(a) * parseInt(b);
+}
+function divide(a,b){
+    return parseInt(a)/parseInt(b);
+}
+let arraySumMinus = [];
+
+function calculate2(expressao){
+    const arr = expressao.split(' ');
+    console.log(arr);
+    //here i solved * and / equations and send it to a new array where there will only be + and -;
+    arr.forEach((item, index) => {
+        if(item == '+'){
+            arraySumMinus.push('+');
+        }
+        if(item == '-'){
+            arraySumMinus.push('-');
+        }
+        if(item == '*'){
+            let resultadoMultiply = multiply(arr[index-1],arr[index+1]);
+            arraySumMinus.push(resultadoMultiply);
+        }
+        if(item == '/'){
+            let resultadoDivide = divide(arr[index-1],arr[index+1]);
+            arraySumMinus.push(resultadoDivide);
+        }
+    });
+    arraySumMinus.forEach((item, index)=>{
+        if(item == '+'){
+            let resultadoSum = add(arr[index-1],arr[index+1]);
+            //stopped here 13:23 - 20/10/23;
+        }
+        if(item == '-'){
+            
+        }
+    })
+}
+
+
 function calculate(expressao){
     try{
         const resultado = eval(expressao);
